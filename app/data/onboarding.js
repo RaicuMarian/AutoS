fadc096e406a95aed4a70af176e763c49d5abbc9
RaicuMarian <raicumarian35@gmail.com>
2022-04-11 22:48:10 +0300

    First commit. Onboarding page already done.

---
+++ app/data/onboarding.js
@@ -0,0 +1,20 @@
+export default data = [
+    {
+        _id: '1',
+        title: 'Nu sta stresat!',
+        description: 'Mergi linistit cu masina la drum stiind ca ai o posibila rezolvare a unei viitoare probleme.',
+        img: require('../assets/images/1.jpg')
+    },
+    {
+        _id : '2',
+        title: 'Cat iti cunosti masina?',
+        description: 'Completeaza formularul cu datele despre masina ta, pentru a avea mai multe sanse de rezolvare!',
+        img: require('../assets/images/2.jpg')
+    },
+    {
+      _id : '3',
+      title: 'Avarie? Apasa S.O.S',
+      description: 'Daca intampini vreo avarie, apasa pe butonul de "S.O.S" si membrii din apropiere vor fi notificati!',
+      img: require('../assets/images/3.jpg')
+    },
+]
